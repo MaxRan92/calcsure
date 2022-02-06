dd5764d39981a50c06548cd5eae60d3938b044a2
Massimo Ranalli <ranallimax@gmail.com>
2022-02-06 09:55:20 +0000

    Add coverage term slider, fix positioning of insurance data and submit button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,19 @@
 
 /*code inspired by https://www.w3schools.com/howto/howto_js_rangeslider.asp */
-var slider = document.getElementById("coverage");
-var output = document.getElementById("coverage-value");
-output.innerHTML = slider.value; // Display the default slider value
+var sliderAmount = document.getElementById("coverage");
+var outputAmount = document.getElementById("coverage-value");
+outputAmount.innerHTML = sliderAmount.value; // Display the default slider value
 
 // Update the current slider value (each time you drag the slider handle)
-slider.oninput = function() {
-  output.innerHTML = this.value;
+sliderAmount.oninput = function() {
+  outputAmount.innerHTML = this.value;
+}
+
+var sliderTerm = document.getElementById("term");
+var outputTerm = document.getElementById("coverage-term");
+outputTerm.innerHTML = sliderTerm.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+sliderTerm.oninput = function() {
+  outputTerm.innerHTML = this.value;
 }
