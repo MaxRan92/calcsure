129e6cbef17ca61f2becb3a1c8220237ef9bf39f
Massimo Ranalli <ranallimax@gmail.com>
2022-02-07 23:56:10 +0000

    insert event listener to submit button

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,3 +17,35 @@ outputTerm.innerHTML = sliderTerm.value; // Display the default slider value
 sliderTerm.oninput = function() {
   outputTerm.innerHTML = this.value;
 }
+
+
+// Wait for the DOM to finish loading before running the calculation
+// Add event listener to the submit button
+document.addEventListener("DOMContentLoaded", function() {
+  let submit = document.getElementById("submit-button");
+  
+  submit.addEventListener("click", calculatePremium);
+   
+  let buttons = document.getElementsByTagName("button");
+  let gender = [];
+
+  for (let button of buttons){
+      button.addEventListener("click", function() {
+          if (this.getAttribute("data-type") === "male") {
+              gender = this.value;
+          }  else  {
+              gender = this.value;
+          }    
+      })
+  }
+  console.log(gender);
+})
+
+
+function calculatePremium() {
+  let age = document.getElementById("age").value;
+  console.log(age);
+  console.log(gender);
+}
+
+
